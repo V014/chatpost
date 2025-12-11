cc75615da3358e653340faebd4f40976c297e617
Wanga Kanjala <wangakanjala@gmail.com>
2025-12-11 22:50:58 +0200

    adjusting layout of content

--- js/core.js
+++ js/core.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
         $("img.tesla").css("cursor","pointer");
         $("img.tesla").css("transition","0.3s");
         $("img.tesla").css("box-shadow","5px 10px 18px #3a3a3a");
-        $("body").css("background-color","#FFCF02");
+        // $("body").css("background-color","#FFCF02");
     })
 
     $('img.plant').hover(function() {
@@ -20,12 +20,12 @@ $(document).ready(function() {
         $("img.plant").css("cursor","pointer");
         $("img.plant").css("transition","0.3s");
         $("img.plant").css("box-shadow","5px 10px 18px #3a3a3a");
-        $("body").css("background-color","#5969B5");
+        // $("body").css("background-color","#5969B5");
     })
 
     $('img').mouseleave(function() {
         $("img").css("width","30%");
         $("img").css("box-shadow","none");
-        $("body").css("background-color","white");
+        // $("body").css("background-color","white");
     })
 });
